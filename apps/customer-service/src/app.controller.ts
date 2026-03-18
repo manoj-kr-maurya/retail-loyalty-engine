c4b6b60ca3abc0e6851788752efa0d178593cbcd
Manoj Kumar <kumar@Manojs-MacBook-Air.local>
2026-03-18 21:53:05 +0530

    fix: customer service fix

--- apps/customer-service/src/app.controller.ts
+++ apps/customer-service/src/app.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
@@ -10,16 +9,5 @@ export class AppController {
   @Get()
   getHello(): Observable<string> {
     return this.appService.getHello();
-  }
-
-  @Get('points/:phone')
-  getPoints(@Param('phone') phone: string) {
-    return this.appService.getPoints(phone);
-  }
-
-  @MessagePattern('points.updated')
-  handlePointsUpdate(@Payload() payload: { phone: string; points: number }) {
-    this.appService.addPoints(payload.phone, payload.points);
-    return { status: 'ok' };
   }
 }
--- apps/customer-service/src/app.module.ts
+++ apps/customer-service/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [AdminModule],
+  imports: [AdminModule, CustomersModule],
   controllers: [AppController],
   providers: [AppService],
 })
